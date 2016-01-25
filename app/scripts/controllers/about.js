65973c5ba74b19cf4582ff7918c44870319a37c6
Dajicu <davidjimenezcuesta@gmail.com>
2016-01-25 18:59:40 +0100

    web instalada

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webZApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the webZApp
+ */
+angular.module('webZApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
